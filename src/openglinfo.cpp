fade629141a0b554ed38435a44c9e43e45e0f9f8
Rohan Garg <rohan@garg.io>
2016-10-18 13:42:45 +0200

    Use the glVendorString from KWin::GLPlatform

--- src/openglinfo.cpp
+++ src/openglinfo.cpp
@@ -36,7 +36,8 @@ OpenGLInfo::OpenGLInfo()
     openGLVersion = QString::fromUtf8(platform->glVersionString());
     mesaVersion = KWin::GLPlatform::versionToString(platform->mesaVersion());
     if (platform->driver() == KWin::Driver::Driver_Unknown) {
-      kwinDriver = (char *) glGetString(GL_VENDOR);
+      KWin::GLPlatform *platform = KWin::GLPlatform::instance();
+      kwinDriver = platform->glVendorString();
     } else {
       kwinDriver = KWin::GLPlatform::driverToString(platform->driver());
     }
